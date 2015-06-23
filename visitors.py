15101656a7565ef83fa994877b0a31b64acdb08c
phoenixits <bounty@phoenixit.org>
2015-06-23 16:35:28 +0200

    Create visitors.py
    chmod 777 visitors.py
    ./visitors.py

---
+++ visitors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#       Title: Apache Visitor Statistics
+#       Author: PhoenixIT
+#       Twitter: @phoenixits
+#       Web: www.phoenixit.org
+#       Date: 23 June 2015
+
+import os
+
+os.system("clear")
+''' banner section '''
+banner = '''
+         #########################################
+         # Server Statistics                     #
+         #########################################
+         '''
+
+print banner
+''' hostname '''
+os.system("hostname")
+print "-" * 20
+''' total unique hits '''
+print "Total Unique Hits: "
+os.system("cat /var/log/apache2/access.log |awk '{print $1}' | sort | uniq | wc -l")
+print "-" * 20
+''' total hits '''
+print "Total Hits: "
+os.system("cat /var/log/apache2/access.log | wc -l")
+print "-" * 20
+''' total unique referrers '''
+print "Unique Referrers: "
+os.system("cat /var/log/apache2/access.log | awk '{print $11};' | awk -F / '{print $3}' | sort | uniq")
